e580787abcf8a25c3408245ccf061f8e96ec4ae7
Pedro Davim <pdavimmilkman@gmail.com>
2019-11-07 10:40:57 +0000

    Update SettingsForm.tsx

--- src/SettingsForm.tsx
+++ src/SettingsForm.tsx
@@ -1,73 +1,82 @@
 // Copyright 2019 The Bytecoin developers.
 // Licensed under the GNU Affero General Public License, version 3.
 
-import React, {useContext, useEffect, useRef} from 'react';
-import * as util from './lib/util';
-import styles from './css/SettingsForm.module.css';
+import React, { useContext, useEffect, useRef } from "react";
+import * as util from "./lib/util";
+import styles from "./css/SettingsForm.module.css";
+
+//react bootstrap elements
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
 
 // @ts-ignore
-import NoSleep from 'nosleep.js';
+import NoSleep from "nosleep.js";
 
 const dayMS = 60 * 60 * 24 * 1000;
 
-const SettingsForm = React.memo((props: {
-  viewOnly: boolean,
-  topBlockHeight: number,
-  topKnownBlockHeight: number,
-  topBlockTime: Date,
-  dismiss: () => void,
-}) => {
-  const syncing = props.topBlockHeight !== props.topKnownBlockHeight;
-  const farBehind = (new Date()).valueOf() - props.topBlockTime.valueOf() > 2 * dayMS;
-  const wantNoSleep = syncing && farBehind && util.isMobile();
+const SettingsForm = React.memo(
+  (props: {
+    viewOnly: boolean;
+    topBlockHeight: number;
+    topKnownBlockHeight: number;
+    topBlockTime: Date;
+    dismiss: () => void;
+  }) => {
+    const syncing = props.topBlockHeight !== props.topKnownBlockHeight;
+    const farBehind =
+      new Date().valueOf() - props.topBlockTime.valueOf() > 2 * dayMS;
+    const wantNoSleep = syncing && farBehind && util.isMobile();
 
-  const wallet = useContext(util.WalletContext);
-  const noSleep = useRef(new NoSleep());
+    const wallet = useContext(util.WalletContext);
+    const noSleep = useRef(new NoSleep());
 
-  useEffect(() => {
-    if (!wantNoSleep) {
-      turnNoSleep(false);
-    }
-  }, [wantNoSleep]);
+    useEffect(() => {
+      if (!wantNoSleep) {
+        turnNoSleep(false);
+      }
+    }, [wantNoSleep]);
 
-  const turnNoSleep = (on: boolean) => {
-    console.info(`no sleep: ${on}`);
+    const turnNoSleep = (on: boolean) => {
+      console.info(`no sleep: ${on}`);
 
-    if (on) {
-      noSleep.current.enable();
-    } else {
-      noSleep.current.disable();
-    }
-  };
+      if (on) {
+        noSleep.current.enable();
+      } else {
+        noSleep.current.disable();
+      }
+    };
 
-  const closeWallet = async () => {
-    if (wallet) {
-      await wallet.close();
-    }
-  };
+    const closeWallet = async () => {
+      if (wallet) {
+        await wallet.close();
+      }
+    };
 
-  const exportMnemonic = async () => {
-    if (wallet === null)  {
-      alert('Wallet closed');
-      return;
-    }
+    const exportMnemonic = async () => {
+      if (wallet === null) {
+        alert("Wallet closed");
+        return;
+      }
 
-    const w = window.open();
-    if (w === null) {
-      alert('Failed to open window with mnemonic text');
-      return;
-    }
+      const w = window.open();
+      if (w === null) {
+        alert("Failed to open window with mnemonic text");
+        return;
+      }
 
-    const [resp, err] = await util.try_(wallet.getWalletInfo({
-      need_secrets: true,
-    }));
-    if (resp === undefined) {
-      w.close();
-      alert(`Failed to get wallet info: ${err}`);
-      return;
-    }
+      const [resp, err] = await util.try_(
+        wallet.getWalletInfo({
+          need_secrets: true
+        })
+      );
+      if (resp === undefined) {
+        w.close();
+        alert(`Failed to get wallet info: ${err}`);
+        return;
+      }
 
-    w.document.write(`
+      w.document.write(`
     <html lang="en">
         <head><title>Bytecoin Mnemonic Backup</title></head>
         <body onafterprint="self.close()">
@@ -75,32 +84,60 @@ const SettingsForm = React.memo((props: {
         </body>
     </html>
     `);
-    w.print();
-  };
+      w.print();
+    };
 
-  return (
-    <div className={styles.settingsForm}>
-      <div className={styles.noSleepGroup}>
-        <input type='checkbox' id='noSleep' onChange={(e) => turnNoSleep(e.target.checked)}/> <label htmlFor='noSleep'>Prevent device sleep during sync</label>
+    return (
+      <div className={styles.settingsForm}>
+        <div className={styles.noSleepGroup}>
+          <Form>
+            <Form.Check
+              type="checkbox"
+              id="noSleep"
+              label="Prevent device sleep during sync"
+              onChange={(event: React.ChangeEvent) => {
+                const castEvent = (event as unknown) as React.ChangeEvent<
+                  HTMLInputElement
+                >;
+                console.log(event);
+                turnNoSleep(castEvent.target.checked);
+              }}
+            />
+          </Form>
+        </div>
+
+        <div className={styles.exportMnemonicGroup}>
+          <Button
+            variant="secondary"
+            className="link-like"
+            onClick={exportMnemonic}
+            disabled={props.viewOnly}
+          >
+            Export mnemonic
+          </Button>{" "}
+          — print or save as PDF
+        </div>
+
+        <div className={styles.controls}>
+          <Button
+            variant="secondary"
+            className={styles.closeWallet}
+            onClick={closeWallet}
+          >
+            Close wallet
+          </Button>
+
+          <Button
+            variant="secondary"
+            className={styles.dismiss}
+            onClick={props.dismiss}
+          >
+            OK
+          </Button>
+        </div>
       </div>
-
-      <div className={styles.exportMnemonicGroup}>
-        <button className='link-like' onClick={exportMnemonic} disabled={props.viewOnly}>
-          Export mnemonic
-        </button> — print or save as PDF
-      </div>
-
-      <div className={styles.controls}>
-        <button className={styles.closeWallet} onClick={closeWallet}>
-          Close wallet
-        </button>
-
-        <button className={styles.dismiss} onClick={props.dismiss}>
-          OK
-        </button>
-      </div>
-    </div>
-  )
-});
+    );
+  }
+);
 
 export default SettingsForm;
